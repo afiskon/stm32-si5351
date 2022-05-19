afiskon/stm32-si5351

commit 08f8ae87965c890c44cb4ad0a853d484b639c351

FIX calculation of t, e.g. using 1048576 i.e. 2^20 causes an overflow for the register using z

si5351/si5351.c
@@ -261,9 +261,7 @@ void si5351_Calc(int32_t Fclk, si5351PLLConfig_t* pll_conf, si5351OutputConfig_t
         x = Fpll/Fclk;
         // t = ceil(Fclk/0xFFFFF)
         t = Fclk >> 20;
-        if(Fclk & 0xFFFFF) {
-            t += 1;
-        }
+        t = (Fxtal >> 20) + 1;
         y = (Fpll % Fclk) / t;
         z = Fclk / t;
     } else {
@@ -282,9 +280,7 @@ void si5351_Calc(int32_t Fclk, si5351PLLConfig_t* pll_conf, si5351OutputConfig_t
         a = numerator/Fxtal;
         // t = ceil(Fxtal/0xFFFFF)
         t = Fxtal >> 20;
-        if(Fxtal & 0xFFFFF) {
-            t += 1;
-        }
+        t = (Fxtal >> 20) + 1;
         b = (numerator % Fxtal) / t;
         c = Fxtal / t;
     }

commit 6206a76ae002f4714dab20949eabb31d0d102af7

s/Fxtal/Fclk, an error introduced in 08f8ae87. Fixes #2

si5351/si5351.c
@@ -259,7 +259,7 @@ void si5351_Calc(int32_t Fclk, si5351PLLConfig_t* pll_conf, si5351OutputConfig_t
         c = 1;
         int32_t Fpll = 900000000;
         x = Fpll/Fclk;
-        t = (Fxtal >> 20) + 1;
+        t = (Fclk >> 20) + 1;
         y = (Fpll % Fclk) / t;
         z = Fclk / t;
     } else {